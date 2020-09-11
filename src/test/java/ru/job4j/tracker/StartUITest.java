cec8fc86e95877eff1f5514a7ef2866621c9b346
saimon494 <saimon.494@gmail.com>
2020-09-11 14:10:35 +0300

    5.1. Тестирование. Подготовка данных. [#353602], исправление

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -38,8 +38,7 @@ public class StartUITest {
         Item item = new Item("new item");
         tracker.add(item);
         String[] answers = {
-                String.valueOf(item.getId()),
-                "delete item"
+                String.valueOf(item.getId())
         };
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
