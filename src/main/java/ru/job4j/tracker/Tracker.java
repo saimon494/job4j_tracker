e8593ce3aa274507ef3f18593dbb1442244b6a30
saimon494 <saimon.494@gmail.com>
2020-09-20 22:05:34 +0300

    6. Изменить программу Tracker из 2-го модуля [#353577], исправление

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Tracker {
@@ -11,7 +10,7 @@ public class Tracker {
 
     public Item add(Item item) {
         item.setId(ids++);
-        items.add(size++, item);
+        items.add(item);
         return item;
     }
 
@@ -21,11 +20,11 @@ public class Tracker {
 
     public List<Item> findByName(String key) {
         List<Item> itemsNames = new ArrayList<>();
-        int index = 0;
+//        int index = 0;
         for (Item item : items) {
             if (item.getName() != null && item.getName().equals(key)) {
-                itemsNames.add(index, item);
-                index++;
+                itemsNames.add(item);
+//                index++;
             }
         }
         return itemsNames;
@@ -33,9 +32,10 @@ public class Tracker {
 
     private int indexOf(int id) {
         int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items.get(index).getId() == id) {
-                rsl = index;
+
+        for (Item item : items) {
+            if (item.getId() == id) {
+                rsl = items.indexOf(item);
                 break;
             }
         }
@@ -53,9 +53,9 @@ public class Tracker {
         item.setId(id);
         if (index != -1) {
             rsl = true;
-            items.add(index, item);
+            items.set(index, item);
         }
-        return rsl;
+              return rsl;
     }
 
     public boolean delete(int id) {
