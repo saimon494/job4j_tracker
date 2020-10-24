db98b7dda0aab139018ffb42cb9765b2933a64ec
saimon494 <saimon.494@gmail.com>
2020-10-24 23:40:21 +0300

    2. Список адресов [#353409]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+public class ProfilesTest {
+
+    @Test
+    public void collectAddress() {
+        List<Profile> profiles = new ArrayList<>();
+        Address address1 = new Address("Moscow", "Lenina", 13, 3);
+        Address address2 = new Address("Piter", "Lenina", 12, 5);
+        Address address3 = new Address("Omsk", "Lenina", 1, 6);
+        profiles.add(new Profile(address1));
+        profiles.add(new Profile(address2));
+        profiles.add(new Profile(address3));
+
+        List<Address> rsl = Profiles.collect(profiles);
+        List<Address> expected = Arrays.asList(address1, address2, address3);
+        assertThat(rsl, is(expected));
+    }
+}
