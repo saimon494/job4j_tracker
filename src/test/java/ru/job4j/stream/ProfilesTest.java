013e46320c5fd6b18e241b985408a2bc6fb0c030
saimon494 <saimon.494@gmail.com>
2020-10-25 00:22:39 +0300

    3. Уникальность элементов и сортировка. [#353410]

--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -12,7 +12,7 @@ import static org.hamcrest.core.Is.is;
 public class ProfilesTest {
 
     @Test
-    public void collectAddress() {
+    public void collect() {
         List<Profile> profiles = new ArrayList<>();
         Address address1 = new Address("Moscow", "Lenina", 13, 3);
         Address address2 = new Address("Piter", "Lenina", 12, 5);
@@ -23,6 +23,21 @@ public class ProfilesTest {
 
         List<Address> rsl = Profiles.collect(profiles);
         List<Address> expected = Arrays.asList(address1, address2, address3);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void collectUnique() {
+        List<Profile> profiles = new ArrayList<>();
+        Address address1 = new Address("Moscow", "Lenina", 13, 3);
+        Address address2 = new Address("Moscow", "Lenina", 13, 3);
+        Address address3 = new Address("Rostov", "Lenina", 1, 6);
+        profiles.add(new Profile(address1));
+        profiles.add(new Profile(address2));
+        profiles.add(new Profile(address3));
+
+        List<Address> rsl = Profiles.collectUnique(profiles);
+        List<Address> expected = Arrays.asList(address1, address3);
         assertThat(rsl, is(expected));
     }
 }
