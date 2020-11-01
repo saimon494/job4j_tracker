737690606c286d6e7273d2f471dd4b16143d5985
saimon494 <saimon.494@gmail.com>
2020-11-01 11:15:28 +0300

    2. Аттестация [#353425]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,49 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+        .mapToInt(Subject::getScore)
+        .average()
+        .orElse(0D);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(pupil.getName(), pupil.getSubjects().stream()
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0D)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, Collectors.averagingDouble(Subject::getScore)))
+                .entrySet().stream()
+                .map(stringDoubleEntry -> new Tuple(stringDoubleEntry.getKey(), stringDoubleEntry.getValue()))
+                .collect(Collectors.toList());
+
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(pupil -> new Tuple(pupil.getName(), pupil.getSubjects().stream()
+        .mapToInt(Subject::getScore)
+        .sum()))
+                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, Collectors.summingInt(Subject::getScore)))
+                .entrySet().stream()
+                .map(stringIntegerEntry -> new Tuple(stringIntegerEntry.getKey(), stringIntegerEntry.getValue()))
+                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(null);
+    }
+}
